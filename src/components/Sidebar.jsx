e8db44fab894798cd81a82a38b3243b57b34b80b
ObiomaIkpe <obiomaikpe1.com>
2024-05-14 09:01:31 +0100

    putting twitter icon in sidebar component

--- src/components/Sidebar.jsx
+++ src/components/Sidebar.jsx
@@ -1,8 +1,12 @@
-
+import Link from 'next/link';
+import {FaXTwitter} from 'react-icons/fa6';
+import {HiHome} from 'react-icons/hi';
 export default function Sidebar() {
   return (
     <div>
-      Sidebar
+      <Link href='/'>
+        <FaXTwitter className='w-16 h-16 cursor-pointer p-3 hover:bg-gray-100 rounded-full transition-all duration-200'/>
+      </Link>
     </div>
   )
 }
